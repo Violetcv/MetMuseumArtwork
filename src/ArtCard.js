6429abcd17a8b16e34854b88e9106424301d96c3
Violetcv <dischhavi@gmail.com>
2023-06-27 23:45:55 +0530

    loading feature and load onscroll added

--- src/ArtCard.js
+++ src/ArtCard.js
@@ -11,7 +11,7 @@ function ArtCard({ title, artistDisplayName, primaryImage, objectID}){
               border: "2px solid #000000"
             }}
           >
-            <img src={primaryImage} className="card-img-top" alt={`Album art of ${title}`} />
+            <img src={primaryImage} className="card-img-top" alt={`Album art of ${title}`} style={{ maxHeight: '200px' }} />
             <div className="card-body d-flex flex-column justify-content-between">
               <h5 className="card-title" style={{ color: "#000000" }}>
                 {title}
--- src/ArtWorks.js
+++ src/ArtWorks.js
@@ -1,108 +1,98 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import ArtCard from './ArtCard';
 import ArtworkInfo from './ArtworkInfo';
 
-//10 - 15 commits; proper git use for projects; initial commit + push
-
 const Artworks = () => {
   const [artworks, setArtworks] = useState([]);
   const [selectArtworks, setSelectedArtworks] = useState();
+  const [page, setPage] = useState(1);
+  const containerRef = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(//different from fetch; syntax; params; query params show up together; json body : http concept : axion and fetch are two librar
+        const response = await axios.get(
           'https://collectionapi.metmuseum.org/public/collection/v1/objects',
           {
             params: {
-              metadataDate: "2023-01-01" //json
+              metadataDate: '2023-06-01',
+              page: page,
+              pageSize: 9 // Display 9 cards per page
             },
           }
         );
 
         const objectIDs = response.data.objectIDs;
-        const firstThreeObjectIDs = objectIDs.slice(0, 3);//on scroll load the next item
-
-        const artworkPromises = firstThreeObjectIDs.map(async (objectID) => {//map: returns an array of promises; setstate: no consistent order
-          const artworkResponse = await axios.get(//await : wait for api to finish then retrieve data
+        const artworkPromises = objectIDs.map(async (objectID) => {
+          const artworkResponse = await axios.get(
             `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`
           );
           return artworkResponse.data;
         });
 
-        const artworksData = await Promise.all(artworkPromises);//loader ( gol gol spin wheel)
-        setArtworks(artworksData);
+        const artworksData = await Promise.all(artworkPromises);
+        setArtworks((prevArtworks) => [...prevArtworks, ...artworksData]);
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [page]);
 
-  useEffect(() => {
-    if (!selectArtworks) {
-      return;
+  const handleScroll = () => {
+    const container = containerRef.current;
+    if (
+      container.scrollTop + container.clientHeight >= container.scrollHeight &&
+      artworks.length > 0
+    ) {
+      setPage((prevPage) => prevPage + 1);
     }
+  };
 
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://collectionapi.metmuseum.org/public/collection/v1/objects/${selectArtworks.objectID}`
-        );
-        const artworkData = response.data;
-        setArtworks([artworkData]);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-  }, [selectArtworks]);
+  const handleClick = (artwork) => {
+    setSelectedArtworks(artwork);
+  };
 
   return (
     <div>
       <h1>Metropolitan Museum Artworks</h1>
-      <div className="container" style={{ border: '1px solid black', padding: '10px' }}>
+      <div
+        className="container"
+        style={{ border: '1px solid black', padding: '10px', maxHeight: '500px', overflowY: 'auto' }}
+        ref={containerRef}
+        onScroll={handleScroll}
+      >
         <div className="row" style={{ display: 'flex', flexWrap: 'wrap' }}>
-          <div className="col-9">
-            <div className="row">
-              {artworks.map((artwork) => (
-                <div
-                className="col-4"
-                style={{ marginBottom: "18px", flex: '0 0 33.33%'  }}
-                onClick={() => setSelectedArtworks(artwork)}
-                key={artwork.objectID}
-                >
-                  <ArtCard
-                    title={artwork.title}
-                    primaryImage={artwork.primaryImage}
-                    artistDisplayName={artwork.artistDisplayName}
-                  />
-                </div>
-              ))}
+          {artworks.map((artwork) => (
+            <div
+              className="col-4"
+              style={{ marginBottom: '18px', flex: '0 0 33.33%' }}
+              onClick={() => handleClick(artwork)}
+              key={artwork.objectID}
+            >
+              <ArtCard
+                title={artwork.title}
+                primaryImage={artwork.primaryImage}
+                artistDisplayName={artwork.artistDisplayName}
+              />
             </div>
-          </div>
-          <div className="col-3" style={{}}>
-            <div className="row">
-              {artworks.map((artwork) => (
-                <ArtworkInfo
-                  key={artwork.objectID}
-                  artistDisplayName={artwork.artistDisplayName}
-                  primaryImage={artwork.primaryImage}
-                  objectID={artwork.objectID}
-                  title={artwork.title}
-                />
-              ))}
-            </div>
-          </div>
+          ))}
         </div>
       </div>
+      {selectArtworks && (
+        <div className="container" style={{ marginTop: '20px' }}>
+          <ArtworkInfo
+            artistDisplayName={selectArtworks.artistDisplayName}
+            primaryImage={selectArtworks.primaryImage}
+            objectID={selectArtworks.objectID}
+            title={selectArtworks.title}
+          />
+        </div>
+      )}
     </div>
   );
 };
 
 export default Artworks;
-
-//has images (search endpoint)
